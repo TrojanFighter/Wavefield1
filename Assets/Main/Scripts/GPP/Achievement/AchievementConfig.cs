bced97a0d28e0ffdc3ab4c961897d3cfe7206feb
Sparky <timdao@hotmail.com>
2018-05-03 01:38:39 -0400

    AchievementTypes

---
+++ Assets/Main/Scripts/GPP/Achievement/AchievementConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName="AchievementRecord")]
+public class AchievementRecord : ScriptableObject
+{
+    public List<AchievementBase> m_Achievements;
+
+}
+
+public class AchievementBase
+{
+    public string AchievementName;
+    public bool BUnlocked;
+}
+
+public class TimesAchievement : AchievementBase
+{
+    public int TotalTimeInNeed;
+    public int CurrentTimes;
+}
+
+public class TypesAchievement : AchievementBase
+{
+    public List<int> AchievementNeededToUnlock;
+}
+
+public enum AchievementTypes
+{
+    A,
+    B,
+    C,
+    D,
+    E
+}
