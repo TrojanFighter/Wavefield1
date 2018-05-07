9f140933f1681c8e0425acd977815a82dcfefdd9
Sparky <timdao@hotmail.com>
2018-05-07 09:46:39 -0400

    Reduce extra code

---
+++ Assets/Main/Scripts/AchievementSystem/Common/Scripts/Achievements/AchievementRules.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using Wavefield.Tools;
+
+namespace WaveField.Achievement
+{
+	/// <summary>
+	/// This class describes how the achievements are triggered.
+	/// It extends the base class MMAchievementRules
+	/// It listens for different event types
+	/// </summary>
+	public class AchievementRules : MMAchievementRules, 
+									MMEventListener<MMGameEvent>, 
+									MMEventListener<AchievementEvent>//,
+
+	{
+
+		public override void OnMMEvent(MMGameEvent gameEvent)
+		{
+
+			base.OnMMEvent (gameEvent);
+
+		}
+
+
+
+		public virtual void OnMMEvent(AchievementEvent achievementEvent)
+		{
+			switch (achievementEvent.EventType)
+			{
+
+				case AchievementEventTypes.PlayerDeath:
+					MMAchievementManager.UnlockAchievement ("DeathIsOnlyTheBeginning");
+					break;
+				case AchievementEventTypes.KillAnyEnemy:
+					MMAchievementManager.UnlockAchievement ("Kill Anyone");
+					break;
+				case AchievementEventTypes.Kill3Enemy:
+					MMAchievementManager.AddProgress("Kill 3 Enemies",1);
+					break;
+				case AchievementEventTypes.ShootBullet:
+					MMAchievementManager.UnlockAchievement ("Shoot a bullet");
+					break;
+			}
+		}
+
+		protected override void OnEnable()
+		{
+			base.OnEnable ();
+			this.MMEventStartListening<MMGameEvent> ();
+			this.MMEventStartListening<AchievementEvent>();
+
+		}
+
+
+		protected override void OnDisable()
+		{
+			base.OnDisable ();
+
+			this.MMEventStopListening<MMGameEvent> ();
+			this.MMEventStopListening<AchievementEvent>();
+
+		}
+
+	}
+}
---
+++ Assets/Main/Scripts/AchievementSystem/Tools/Achievements/Scripts/Define/AchievementDefine.cs
@@ -0,0 +1,38 @@
+﻿namespace WaveField.Achievement
+{
+	/// <summary>
+	/// A list of the possible base events
+	/// </summary>
+	public enum AchievementEventTypes
+	{
+		LevelStart,
+		LevelComplete,
+		LevelEnd,
+		Pause,
+		UnPause,
+		PlayerDeath,
+		Respawn,
+		KillAnyEnemy,
+		Kill3Enemy,
+		ShootBullet
+	}
+
+	/// <summary>
+	/// A type of events used to signal level start and end (for now)
+	/// </summary>
+	public struct AchievementEvent
+	{
+		public AchievementEventTypes EventType;
+
+		/// <summary>
+		/// Initializes a new instance of the Achievement struct.
+		/// </summary>
+		/// <param name="eventType">Event type.</param>
+		public AchievementEvent(AchievementEventTypes eventType)
+		{
+			EventType = eventType;
+		}
+	}
+	
+	
+}
